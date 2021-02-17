dfc10ee385458c97846093ad2d30dc8c23d90675
fennecfox38 <fennecfox38@gmail.com>
2021-02-17 18:48:03 +0900

    Update Comment

--- gray.c
+++ gray.c
@@ -1,5 +1,11 @@
+/************************How to make it gray-scale************************
+ * 1. Add up values of each color channel.
+ * 2. Get average. It is Brightness of pixel.
+ * 3. Assign 'Brightness' to each channel (regardless which channel)
+ *************************************************************************/
 #include "bitmap.h"
 
+// Below are gray-scale logic for each bit size and format.
 void grayRGB16565(void* p){
     unsigned short gray = ( ((RGB16_565*)p)->red + ((((RGB16_565*)p)->green)>>1) + ((RGB16_565*)p)->blue ) /3;
     ((RGB16_565*)p)->red=((RGB16_565*)p)->blue=gray;
@@ -19,32 +25,35 @@ void grayQuad(void *p){
 }
 
 int gray(char* src, char* dst){
-    IMAGE img;
+    IMAGE img;  // Container for Abstracted info and data of BMP file
     
     if(!readImage(&img,src)) return 0;
 
     void (*func)(void*);
     switch(img.bi.bitCount){
-        case 1: case 4: case 8: 
+        case 1: case 4: case 8:     // if it has color table, just process color table only.
             for(RGBQUAD* p=((RGBQUAD*)(img.extra))+(img.bi.clrUsed-1);p>=(RGBQUAD*)(img.extra);--p)
                 grayQuad(p);
-            goto TERMINATE; break;
+            goto TERMINATE; break;  // skipping Pixel Array traversal.
+
+        // Assiging function by color depth.
         case 16: func=(img.bi.compression==0 ? grayRGB16555 : grayRGB16565); break;
         case 24: func=grayTriple; break;
         case 32: func=grayQuad; break;
-        default: goto ERROR; break;
+        default: goto ERROR; break; // ???: Error!
     }
 
+    // Pixel Array Traversal by assigned function.
     for (int y=img.bi.height-1; y>=0; --y)
         for (int x=img.bi.width-1; x>=0; --x)
             func(PIXEL(img,x,y));
 
-TERMINATE:
+TERMINATE:  // Write output before termination.
     if(!writeImage(&img,dst)) goto ERROR;
     freeImage(&img);
-    return 1;
+    return 1;   // Succeed
 
-ERROR:
+ERROR:      // free allocated memory even if failed.
     freeImage(&img);
-    return 0;
+    return 0;   // failed in some reason.
 }
--- invert.c
+++ invert.c
@@ -1,5 +1,10 @@
+/******************************How to invert pixel******************************
+ * Invert valid bit. (but no matter even if it is invalid because it is invalid)
+ *******************************************************************************/
+
 #include "bitmap.h"
 
+// Below are inversion logic for each bit size and format.
 void invRGB16(void* p){ *((unsigned short*)p)=~*((unsigned short*)p); }
 void invTriple(void* p){
     ((RGBTRIPLE*)p)->rgbtRed=~(((RGBTRIPLE*)p)->rgbtRed);
@@ -14,32 +19,35 @@ void invQuad(void* p){
 }
 
 int invert(char* src, char* dst){
-    IMAGE img;
+    IMAGE img;  // Container for Abstracted info and data of BMP file
     
     if(!readImage(&img,src)) return 0;
     
     void (*inv)(void*);
     switch(img.bi.bitCount){
-        case 1: case 4: case 8: 
+        case 1: case 4: case 8:     // if it has color table, just process color table only.
             for(RGBQUAD* p=((RGBQUAD*)(img.extra))+(img.bi.clrUsed-1);p>=(RGBQUAD*)(img.extra);--p)
                 invQuad(p);
-        goto TERMINATE; break;
+            goto TERMINATE; break;  // skipping Pixel Array traversal.
+
+        // Assiging function by color depth.
         case 16: inv=invRGB16; break;
         case 24: inv=invTriple; break;
         case 32: inv=invQuad; break;
-        default: goto ERROR; break;
+        default: goto ERROR; break; // ???: Error!
     }
 
+    // Pixel Array Traversal by assigned function.
     for (int y=img.bi.height-1; y>=0; --y)
         for (int x=img.bi.width-1; x>=0; --x)
-            inv(PIXEL(img,x,y));
-    
-TERMINATE:
+            func(PIXEL(img,x,y));
+
+TERMINATE:  // Write output before termination.
     if(!writeImage(&img,dst)) goto ERROR;
     freeImage(&img);
-    return 1;
+    return 1;   // Succeed
 
-ERROR:
+ERROR:      // free allocated memory even if failed.
     freeImage(&img);
-    return 0;
+    return 0;   // failed in some reason.
 }
